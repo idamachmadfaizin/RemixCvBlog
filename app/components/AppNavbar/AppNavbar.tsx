52624267d4feba5321b449fedace7d5993c0d21e
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2024-11-07 08:10:17 +0000

    refactor(component): remove header tag in AppNavbar.

--- app/components/AppNavbar/AppNavbar.tsx
+++ app/components/AppNavbar/AppNavbar.tsx
@@ -1,38 +1,33 @@
 import { Link } from "@remix-run/react";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 import { AppThemeModeSwitch } from "../AppThemeMode/AppThemeModeSwitch";
+import { HomeIcon } from "@heroicons/react/24/outline";
+import { AppIcon } from "../AppIcon/AppIcon";
 
-type Props = React.ComponentPropsWithoutRef<"header">;
+type Props = React.ComponentPropsWithoutRef<"nav">;
 
-export const AppNavbar: React.FC<Props> = (props) => {
-  props;
+export const AppNavbar: React.FC<Props> = ({ className, ...props }) => {
   return (
-    <header {...props}>
-      <nav className="py-2.5 lg:px-6">
-        <div className="mx-auto flex flex-wrap items-center justify-between">
-          <Link to="/" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              width="24"
-              height="24"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-              Home
-            </span>
+    <nav className={twMerge("py-2.5 lg:px-6", className)} {...props}>
+      <div className="mx-auto flex flex-wrap items-center justify-between">
+        <Link
+          to="/"
+          className="flex items-center"
+          aria-label="Go to home route"
+        >
+          <AppIcon icon={HomeIcon} className="size-6 stroke-2" />
+        </Link>
+        <div className="flex gap-x-1">
+          <Link
+            to={`about`}
+            className="rounded-lg px-4 py-2 text-sm font-medium text-gray-800 hover:text-primary-500 lg:px-5 lg:py-2.5 dark:text-white"
+          >
+            About
           </Link>
-          <div className="flex gap-x-1">
-            <Link
-              to={`about`}
-              className="rounded-lg px-4 py-2 text-sm font-medium text-gray-800 hover:text-primary-500 lg:px-5 lg:py-2.5 dark:text-white"
-            >
-              About
-            </Link>
-            <AppThemeModeSwitch />
-          </div>
+          <AppThemeModeSwitch />
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
   );
 };
