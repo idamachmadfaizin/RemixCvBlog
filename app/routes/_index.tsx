8ea96acc046516505105da0e2ca8ea592142147c
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2024-11-07 08:25:16 +0000

    feat: Redirect root to about page.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,3 @@
-import type { MetaFunction } from "@remix-run/node";
-import { AppNavbar } from "~/components/AppNavbar/AppNavbar";
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-export default function Index() {
-  return (
-    <main className="container mx-auto min-h-screen max-w-screen-xl px-6 sm:px-10">
-      <AppNavbar className="mb-12" />
-    </main>
-  );
-}
+export const loader = () => redirect("/about");
